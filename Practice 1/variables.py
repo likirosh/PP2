f5c85354cb81223e9562b89e437bb01747a2365f
fluffy <fult3847@gmail.com>
2026-01-30 20:34:38 +0500

    week1

---
+++ Practice 1/variables.py
@@ -0,0 +1,29 @@
+#example 1
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#example 2
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#example 3
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#example 4
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#example 5
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
